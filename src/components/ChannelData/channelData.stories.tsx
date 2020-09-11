273c9e30c2c08ff764dab9ffd6672157616bbd33
Paulo Luan <pauloluan.inova@gmail.com>
2020-09-11 04:22:53 -0300

    📦 add: added initial story file for channelData

---
+++ src/components/ChannelData/channelData.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ChannelData } from '.'
+
+export default {
+  title: 'ChannelData',
+  component: ChannelData
+}
+
+export const OulinedChannelData = () => <ChannelData />
+
+export const NotOulinedChannelData = () => <ChannelData />
