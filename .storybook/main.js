3281c07bf90f277d179affe9ceb41844ada71aaf
Paulo Luan <pauloluan.inova@gmail.com>
2020-12-03 15:38:15 -0300

    bkp

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,14 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-essentials', '@storybook/preset-create-react-app']
+  addons: [
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/preset-create-react-app',
+      options: {
+        scriptsPackageName: path.resolve('../../../node_modules/.bin/react-scripts')
+      }
+    }
+  ]
 }
